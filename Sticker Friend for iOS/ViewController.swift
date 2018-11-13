cd14907e40cd56fbb9878db1c25b6e3ec8f25cf5
João Leite <joaopleite26@gmail.com>
2018-11-13 21:59:49 -0200

    Initial Commit

---
+++ Sticker Friend for iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sticker Friend for iOS
+//
+//  Created by João Leite on 13/11/18.
+//  Copyright © 2018 João Leite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
